bdc2b7b6b8b052d8f1ac42d47a73635be26b3d4d
Aref-H <aref13808@gmail.com>
2023-05-22 22:44:41 +0330

    Including needed libraries

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    char name[10];
+    getch();
+
+    printf("Enter your name :\n");
+    fgets(name, 9, stdin);
+    printf("%s", name);
+
     return 0;
 }
+
